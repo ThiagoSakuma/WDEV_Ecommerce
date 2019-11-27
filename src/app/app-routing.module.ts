cb91482107e9f9774dfa5d12c3cc8ded80f495f7
ThiagoSakuma <thiagosakuma_l_@hotmail.com>
2019-11-27 10:07:17 -0200

    home_components_ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {LancamentosComponent} from './lancamentos/lancamentos.component';
 
 const routes: Routes = [
   {
-    path: "/home",
+    path: 'home',
     component: HomeComponent
   },
   {
-    path: "",
+    path: '',
     component: HomeComponent
   },
   {
-    path: "/lancamentos",
+    path: 'lancamentos',
     component: LancamentosComponent
   }
 ];

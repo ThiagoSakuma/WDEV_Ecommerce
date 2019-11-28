fd1cb02d8ca3c89a6c7c712d8ba1e61045ba0597
ThiagoSakuma <thiagosakuma_l_@hotmail.com>
2019-11-28 12:23:32 -0200

    aula dia 28

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,9 +12,10 @@ export class HomeComponent implements OnInit {
 
   constructor() { 
     this.produtos=[
-      new Produto('Caneta',10.00),
-      new Produto('Caderno',15.00),
-      new Produto('Caneca',5.00)
+      new Produto('Caneta',10.00,'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQi7kOsF3PBuQRduaOqtbvUlVCFtBLUP2DVo1d6Mt6fdpVNuOjN'),
+      new Produto('Caderno',15.00,'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQbaO4lnFv4oMh8feM1-oY1cA8FSwn5Syb0Ucm1XU6JaftAWMQ4'),
+      new Produto('Pendrive',5.00,'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRDhvt_d-IhRNNSWgVFAy4hWO7R3YWOQ8ykCeqLwSAVlZSq8prT'),
+      new Produto('SSD',75.00,'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQv0uvxCRisqhb1ZOby7gDNLOOcMeDt_ySfNTlIuDwUKxEZdPfs')
     ];
   }
 
